08289c71fc7f01456ac318da0e5628c463d6ec0b
riansyahrobi8 <robidahariansyah8@gmail.com>
2020-08-10 21:54:30 +0700

    adding component EntryForm

--- lib/Home.dart
+++ lib/Home.dart
@@ -1,16 +1,112 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_with_data/components/EntryForm.dart';
+import 'package:flutter_with_data/helper/DBHelper.dart';
+import 'package:flutter_with_data/models/Contact.dart';
+import 'package:sqflite/sqflite.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  DBHelper dbHelper;
+  int count = 0;
+  List<Contact> contactList;
+
   @override
   Widget build(BuildContext context) {
+    if (contactList == null) {
+      contactList = List<Contact>();
+    }
+
     return Scaffold(
       appBar: AppBar(
-        title: Text("Flutter with DB"),
+        title: Text("Daftar data-data"),
         leading: Icon(Icons.menu),
       ),
-      body: Center(
-        child: Text("Center"),
+      body: createListView(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          var contact = await navigateToEntryForm(context, null);
+          if (contact != null) addContact(contact);
+        },
+        tooltip: 'Tambah data',
+        child: Icon(Icons.add),
       ),
     );
+  }
+
+  Future<Contact> navigateToEntryForm(
+      BuildContext context, Contact contact) async {
+    var result = await Navigator.push(context,
+        MaterialPageRoute(builder: (BuildContext context) {
+      return EntryForm(contact);
+    }));
+    return result;
+  }
+
+  ListView createListView() {
+    TextStyle textStyle = Theme.of(context).textTheme.subhead;
+
+    return ListView.builder(itemBuilder: (BuildContext context, int index) {
+      return Card(
+        color: Colors.white,
+        elevation: 2.0,
+        child: ListTile(
+          leading: CircleAvatar(
+            backgroundColor: Colors.red,
+            child: Icon(Icons.people),
+          ),
+          title: Text(this.contactList[index].name, style: textStyle),
+          subtitle: Text(
+            this.contactList[index].phone,
+          ),
+          trailing: GestureDetector(
+            child: Icon(Icons.delete),
+            onTap: () {
+              deleteContact(contactList[index]);
+            },
+          ),
+          onTap: () async {
+            var contact =
+                await navigateToEntryForm(context, this.contactList[index]);
+            if (contact != null) editContact(contact);
+          },
+        ),
+      );
+    });
+  }
+
+  void addContact(Contact contact) async {
+    int result = await dbHelper.insert(contact);
+    if (result > 0) {
+      updateLIstView();
+    }
+  }
+
+  void editContact(Contact contact) async {
+    int result = await dbHelper.update(contact);
+    if (result > 0) {
+      updateLIstView();
+    }
+  }
+
+  void deleteContact(Contact contact) async {
+    int result = await dbHelper.delete(contact.id);
+    if (result > 0) updateLIstView();
+  }
+
+  void updateLIstView() {
+    final Future<Database> dbFuture = dbHelper.initDb();
+    dbFuture.then((database) {
+      Future<List<Contact>> contactListFuture = dbHelper.getContactList();
+      contactListFuture.then((contactList) {
+        setState(() {
+          this.contactList = contactList;
+          this.count = contactList.length;
+        });
+      });
+    });
   }
 }
---
+++ lib/components/EntryForm.dart
@@ -0,0 +1,117 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_with_data/models/Contact.dart';
+
+class EntryForm extends StatefulWidget {
+  EntryForm(this.contact);
+
+  final Contact contact;
+
+  @override
+  _EntryFormState createState() => _EntryFormState(this.contact);
+}
+
+class _EntryFormState extends State<EntryForm> {
+  _EntryFormState(this.contact);
+
+  Contact contact;
+
+  TextEditingController nameController = TextEditingController();
+  TextEditingController phoneController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    if (contact != null) {
+      nameController.text = contact.name;
+      phoneController.text = contact.phone;
+    }
+
+    return Scaffold(
+      appBar: AppBar(
+        title: contact == null ? Text('Tambah') : Text('Ubah'),
+        leading: Icon(Icons.keyboard_arrow_left),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.only(top: 15.0, left: 10.0, right: 10.0),
+        child: ListView(children: <Widget>[
+          // name
+          Padding(
+            padding: const EdgeInsets.only(top: 20.0, bottom: 20.0),
+            child: TextField(
+              controller: nameController,
+              onChanged: (value) {
+                // do someting
+              },
+              keyboardType: TextInputType.text,
+              decoration: InputDecoration(
+                  labelText: 'Nama Lengkap',
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(5.0))),
+            ),
+          ),
+
+          // phone
+          Padding(
+            padding: const EdgeInsets.only(top: 20.0, bottom: 20.0),
+            child: TextField(
+              controller: phoneController,
+              onChanged: (value) {
+                // do something
+              },
+              keyboardType: TextInputType.text,
+              decoration: InputDecoration(
+                  labelText: 'Telepon',
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(5.0))),
+            ),
+          ),
+
+          // tombol button
+          Padding(
+            padding: const EdgeInsets.only(top: 20.0, bottom: 20.0),
+            child: Row(
+              children: [
+                Expanded(
+                    child: RaisedButton(
+                  onPressed: () {
+                    if (contact == null) {
+                      // insert data
+                      contact =
+                          Contact(nameController.text, phoneController.text);
+                    } else {
+                      // edit data
+                      contact.name = nameController.text;
+                      contact.phone = phoneController.text;
+                    }
+
+                    Navigator.pop(context, contact);
+                  },
+                  color: Theme.of(context).primaryColorDark,
+                  textColor: Theme.of(context).primaryColorLight,
+                  child: Text(
+                    'Save',
+                    textScaleFactor: 1.5,
+                  ),
+                )),
+                Container(
+                  width: 5.0,
+                ),
+                Expanded(
+                    child: RaisedButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  color: Theme.of(context).primaryColorDark,
+                  textColor: Theme.of(context).primaryColorLight,
+                  child: Text(
+                    'Cancel',
+                    textScaleFactor: 1.5,
+                  ),
+                ))
+              ],
+            ),
+          )
+        ]),
+      ),
+    );
+  }
+}
